f5d2ba30d4c131966479a5f71fc55e5d1b332ed7
cyberdrish <drishmalhotra1997@gmail.com>
2025-08-14 12:55:36 +0530

    init

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,9 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://zfxexqqizllmktyysbhe.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpmeGV4cXFpemxsbWt0eXlzYmhlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA3MTg4MjYsImV4cCI6MjA0NjI5NDgyNn0.2ETYPjVCBHcIOW1CypF7780feeiJUKhjc_zUqPmYFco";
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+
+const supabase = createClient(supabaseUrl, import.meta.env.VITE_SUPABASE_KEY);
 
 export default supabase;
 export { supabaseUrl };
